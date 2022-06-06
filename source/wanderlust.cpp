8fda94b95e8c1f76957fd8b484e76317810a9c35
Scragg <danielscragg@outlook.com>
2022-06-07 01:32:51 +0200

    texture, fire, collision list

--- source/wanderlust.cpp
+++ source/wanderlust.cpp
@@ -13,6 +13,20 @@
 
 #define LOG(x) std::cout << x << std::endl;
 
+// Global variables
+const int unwalkableSize = 8;
+
+// Helper functions
+bool isUnwalkable(const sf::Texture* texture, sf::Texture** list) {
+    for (int i = 0; i < unwalkableSize; i++) {
+        if (texture == list[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Main function
 int main()
 {
     // -------------- SETUP --------------
@@ -28,6 +42,10 @@ int main()
 
     // Variables
     sf::Vector2f center = sf::Vector2f(window.getSize().x / 2.f, window.getSize().y / 2.f);
+    int tick = 0;
+    sf::Texture** unwalkable = new sf::Texture * [unwalkableSize];
+    int sceneX = 0;
+    int sceneY = 0;
     
     // -----------------------------------
 
@@ -55,9 +73,11 @@ int main()
     
     sf::Texture waterTile1;
     waterTile1.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((0 * 16) + 0 , (0 * 16) + 0), sf::Vector2i(16, 16)));
+    unwalkable[0] = &waterTile1;
     
     sf::Texture waterTile2;
     waterTile2.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((1 * 16) + 1 , (0 * 16) + 0), sf::Vector2i(16, 16)));
+    unwalkable[1] = &waterTile2;
     
     sf::Texture dirtTile1;
     dirtTile1.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((6 * 16) + 6 , (0 * 16) + 0), sf::Vector2i(16, 16)));
@@ -73,18 +93,27 @@ int main()
     
     sf::Texture tentTile1;
     tentTile1.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((46 * 16) + 46 , (10 * 16) + 10), sf::Vector2i(16, 16)));
-    
+    unwalkable[2] = &tentTile1;
+
     sf::Texture tentTile2;
     tentTile2.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((47 * 16) + 47 , (10 * 16) + 10), sf::Vector2i(16, 16)));
-    
+    unwalkable[3] = &tentTile2;
+
     sf::Texture tentTile3;
     tentTile3.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((46 * 16) + 46 , (11 * 16) + 11), sf::Vector2i(16, 16)));
-    
+    unwalkable[4] = &tentTile3;
+
     sf::Texture tentTile4;
     tentTile4.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((47 * 16) + 47 , (11 * 16) + 11), sf::Vector2i(16, 16)));
-    
-    sf::Texture campfireTile;
-    campfireTile.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((15 * 16) + 15 , (8 * 16) + 8), sf::Vector2i(16, 16)));
+    unwalkable[5] = &tentTile4;
+
+    sf::Texture campfireTile1;
+    campfireTile1.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((15 * 16) + 15 , (8 * 16) + 8), sf::Vector2i(16, 16)));
+    unwalkable[6] = &campfireTile1;
+
+    sf::Texture campfireTile2;
+    campfireTile2.loadFromFile(spritesheet, sf::IntRect(sf::Vector2i((14 * 16) + 14, (8 * 16) + 8), sf::Vector2i(16, 16)));
+    unwalkable[7] = &campfireTile2;
     
     // -----------------------------------
 
@@ -135,46 +164,62 @@ int main()
             if(token == "G"){  
                 if(randNum == 1){
                     Tiles[tileCounter].setTexture(&grassTile1);
-                }else if(randNum == 2){
+                }
+                else if(randNum == 2){
                     Tiles[tileCounter].setTexture(&grassTile2);
                 }
-            }else if(token == "D"){
+            }
+            else if(token == "D"){
                 if(randNum == 1){
                     Tiles[tileCounter].setTexture(&dirtTile1);
-                }else if(randNum == 2){
+                }
+                else if(randNum == 2){
                     Tiles[tileCounter].setTexture(&dirtTile2);
                 }
-            }else if(token == "W"){
+            }
+            else if(token == "W"){
                 if(randNum == 1){
                     Tiles[tileCounter].setTexture(&waterTile1);
-                }else if(randNum == 2){
+                }
+                else if(randNum == 2){
                     Tiles[tileCounter].setTexture(&waterTile2);
                 }
-            }else if(token == "S"){
+            }
+            else if(token == "S"){
                 if(randNum == 1){
                     Tiles[tileCounter].setTexture(&stoneTile1);
-                }else if(randNum == 2){
+                }
+                else if(randNum == 2){
                     Tiles[tileCounter].setTexture(&stoneTile2);
                 }
-            }else if(token == "0"){
+            }
+            else if(token == "0"){
                 Tiles[tileCounter].setTexture(&tentTile1); 
-            }else if(token == "1"){
+            }
+            else if(token == "1"){
                Tiles[tileCounter].setTexture(&tentTile2); 
-            }else if(token == "2"){
+            }
+            else if(token == "2"){
                 Tiles[tileCounter].setTexture(&tentTile3); 
-            }else if(token == "3"){
+            }
+            else if(token == "3"){
                 Tiles[tileCounter].setTexture(&tentTile4); 
-            }else if(token == "F"){
-                Tiles[tileCounter].setTexture(&campfireTile);
+            }
+            else if(token == "F"){
+                if (randNum == 1) {
+                    Tiles[tileCounter].setTexture(&campfireTile1);
+                }
+                else if (randNum == 2) {
+                    Tiles[tileCounter].setTexture(&campfireTile2);
+                }
             }
 
             tileCounter = tileCounter + 1;
             c = c + 1;
-        }else{
-            //if(tileCounter != 0){
-                c = 0;
-                r++;
-            //}
+        }
+        else{
+            c = 0;
+            r++;
         }
     }
     mapFile.close();
@@ -200,6 +245,7 @@ int main()
     {   
         // Tick Clock
         sf::Time dt = deltaTime.restart();
+        tick++;
 
         // ---------- HANDLE EVENTS ----------
         sf::Event event;
@@ -207,7 +253,8 @@ int main()
         {
             if (event.type == sf::Event::Closed){
                 window.close();
-            }else if (event.type == sf::Event::KeyPressed){
+            }
+            else if (event.type == sf::Event::KeyPressed){
                 if(event.key.code == sf::Keyboard::Escape){
                     window.close();
                     return 0;
@@ -221,13 +268,16 @@ int main()
         if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up)){
             player.Move(sf::Vector2f(0.f, -player.speed * dt.asSeconds() * 100.f));
             player.setTexture(&playerUp);
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)){
             player.Move(sf::Vector2f(0.f, player.speed * dt.asSeconds() * 100.f));
             player.setTexture(&playerDown);
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
             player.Move(sf::Vector2f(-player.speed * dt.asSeconds() * 100.f, 0.f));
             player.setTexture(&playerLeft);
-        }else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
+        }
+        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)){
             player.Move(sf::Vector2f(player.speed * dt.asSeconds() * 100.f, 0.f));
             player.setTexture(&playerRight);
         }
@@ -235,15 +285,18 @@ int main()
 
         // ------------ COLLISONS ------------
         for(int i=0;i<numTiles;i++){
-            if(Tiles[i].getTexture() == &waterTile1 || Tiles[i].getTexture() == &waterTile2){
+            if(isUnwalkable(Tiles[i].getTexture(), unwalkable)){
                 if(player.feetCollider.getGlobalBounds().intersects(Tiles[i].getGlobalBounds())){
                     if(player.position.x < Tiles[i].getPosition().x){
                         player.setPosition(sf::Vector2f(Tiles[i].getPosition().x - 12.f, player.position.y));
-                    }else if(player.position.x > Tiles[i].getPosition().x + 48.f){
+                    }
+                    else if(player.position.x > Tiles[i].getPosition().x + 48.f){
                         player.setPosition(sf::Vector2f(Tiles[i].getPosition().x + 60.f, player.position.y));
-                    }else if(player.position.y < Tiles[i].getPosition().y){
+                    }
+                    else if(player.position.y < Tiles[i].getPosition().y){
                         player.setPosition(sf::Vector2f(player.position.x, Tiles[i].getPosition().y - 24.f));
-                    }else if(player.position.y > Tiles[i].getPosition().y + 19.f){
+                    }
+                    else if(player.position.y > Tiles[i].getPosition().y + 19.f){
                         player.setPosition(sf::Vector2f(player.position.x, Tiles[i].getPosition().y + 29.f));
                     }
                 }
@@ -253,24 +306,46 @@ int main()
 
         // ----------- MOVE SCENES -----------
         if( player.position.x > window.getSize().x){
+            sceneX++;
+            cout << "X: " << sceneX << " | Y: " << sceneY << endl;
             player.position.x -= window.getSize().x;
             for(int i=0;i<numTiles;i++){
                 Tiles[i].setPosition(Tiles[i].getPosition().x - window.getSize().x, Tiles[i].getPosition().y);
             }
-        }else if( player.position.x < 0){
+        }
+        else if( player.position.x < 0){
+            sceneX--;
+            cout << "X: " << sceneX << " | Y: " << sceneY << endl;
             player.position.x += window.getSize().x;
             for(int i=0;i<numTiles;i++){
                 Tiles[i].setPosition(Tiles[i].getPosition().x + window.getSize().x, Tiles[i].getPosition().y);
             }
-        }else if( player.position.y > window.getSize().y){
+        }
+        else if( player.position.y > window.getSize().y){
+            sceneY++;
+            cout << "X: " << sceneX << " | Y: " << sceneY << endl;
             player.position.y -= window.getSize().y;
             for(int i=0;i<numTiles;i++){
                 Tiles[i].setPosition(Tiles[i].getPosition().x, Tiles[i].getPosition().y - window.getSize().y);
             }
-        }else if( player.position.y < 0){
+        }
+        else if( player.position.y < 0){
+            sceneY--;
+            cout << "X: " << sceneX << " | Y: " << sceneY << endl;
             player.position.y += window.getSize().y;
             for(int i=0;i<numTiles;i++){
                 Tiles[i].setPosition(Tiles[i].getPosition().x, Tiles[i].getPosition().y + window.getSize().y);
+            }
+        }
+        // -----------------------------------
+
+        // ---------- UPDATE OBJECTS ---------
+        for (int i = 0; i < numTiles; i++) {
+            if (Tiles[i].getTexture() == &campfireTile1 && tick % 20 == 0) {
+                Tiles[i].setTexture(&campfireTile2);
+            }
+            else if (Tiles[i].getTexture() == &campfireTile2 && tick % 20 == 0) {
+                Tiles[i].setTexture(&campfireTile1);
             }
         }
         // -----------------------------------
